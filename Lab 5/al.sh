32772f430e0c0e19374fbcb488e1a52a74834244
Sudev <sudevdev@gmail.com>
2012-09-15 02:20:57 +0530

    lab5

---
+++ Lab 5/al.sh
@@ -0,0 +1,7 @@
+bash --rcfile <(echo "PS1=$1"
+echo 'alias month="bash ./half.c"'
+echo 'alias greet="bash ./double.c"'
+echo 'alias twice="bash ./twice.c"'
+)
+#PS1=$1; export PS1
+
--- Lab 5/half.c
+++ Lab 5/half.c
@@ -3,7 +3,31 @@
 
 int main(int argc, char * argv[])
 {
-	printf("asdsadasdasd");
-	//execl( argv, argv, (char *)0);
+  if(argc==1)
+    {
+    printf("Too few arguments");
+    return 1;
+    }
+  double ans=atoi(argv[argc-1],"%f",&ans);
+  if (ans==0)
+   {
+     printf("Last argument is not a valid number");
+     return 1;
+   }
+    ans=ans*ans*ans;
+  printf("Process id of cube : %d\n", getpid());
+  if(argc==2)
+    {
+    printf("the answer is %f",ans);
+    return(0);
+    }
+  else
+  {	
+    sprintf(argv[argc-1], "%f", ans);
+    if(execvp(argv[1], &argv[1])==-1)
+    printf("error while trying to excep");
+  }
+  return 1;
 }
+
 
