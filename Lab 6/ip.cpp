4b537b3e06168dc87b2c3686fc84c85feb8e918d
Sudev <sudevdev@gmail.com>
2012-09-23 12:08:38 +0530

    working

--- Lab 6/ip.cpp
+++ Lab 6/ip.cpp
@@ -6,18 +6,24 @@ using namespace std;
 //universal array containing all the info 
 int array[10][10]={{},{}};
 //PCB for process 
-int ptime,noofprocess;
+int iowait=0;
+int * readyqueue();
 int over();
 void readandsave();
+void ioqueuer(int *,int *);
+int ptime=0,noofprocess;
+//keeps track of the current process 
+
+int current=0;
+int printarray[1000];
 struct process
 {
 	int cpu[10];
 	int io[10];
 	int arival;
-	int * current;
 	int numcpu;
 	int numio;
-	int status[3];
+	int signal[3];
 	int currentcpu;
 	int currentio;
 	//the status array is used to get the status of any process which 
@@ -108,39 +114,107 @@ for(int a=0;a<nopt;a++)
 }
 
 }
+
+
+
 int main () 
 {
 	readandsave();
-	//we have all the details of the process the process structure now we have to 
+	//we have all the details of the process t 	he process structure now we have to 
 	//manipulate the data using suitable methods 
 //time 
+//print point 
+int pp=0;
 
-ptime =0;
 //select the process with least cpu burst 
 while(over())
 	{	
-	readyqueue();
-	//increment time 
+	int * tem = readyqueue();
+	//cout<<endl<<"process to be executed is "<<*tem;
+	//now to take record of all the happenings
+	/* trying to keep print the whole stuff */
+	cout << "the process executed at time\t " <<ptime<<"\twas\t"<<current+1<<endl;
+	/*if(ptime==0)
+	{ 
+		printarray[pp] = current;
+		printarray[pp+1] = 1;
+	}
+	else
+	{
+		if(printarray[pp - 1] == current )
+			{
+			printarray[pp]++;
+			}
+		else
+			{
+			pp++;
+			printarray[pp] = current;
+			pp++;
+			printarray[pp] = 1;
+			}
+	}
+	for(int i; i < pp+1;i=i+2)
+		cout<<printarray[i]<<" : "<<printarray[i+1]<<"\t";
+	*/
+	(*tem)--;
 	ptime++;
+	
 	}
 }
+
+
 
 //function to check completion of all the process 
 int over()
 {
-		
-		return 0;
-}
-int readyqueue()
-{
-	int * jobqueue[10];
-	for(int i=0;i<noofprocess;i++)
+	for(int i =0 ; i < noofprocess; i++)
 	{
-	if(proc[i].numcpu < proc[i].cpu[currentcpu] )
-		proc[i].signal[1]=-1;
-	if(proc[i].arival < ptime && proc[i].signal[1] != -1)
+		if(proc[i].signal[1] != -1)
 		{
-		jobqueue[i] = &proc[i].cpu[currentcpu];
+		return 1;
 		}
 	}
+	return 0;
+}
+int * readyqueue()
+{
+	//to find the minimum 
+	int * temp = NULL;
+	for(int i=0;i<noofprocess;i++)
+	{
+	//checking for the completion of the process 
+	if(proc[i].cpu[proc[i].currentcpu] <=0)
+		{
+		if(proc[i].numcpu>proc[i].currentcpu )
+			proc[i].currentcpu++;
+			//calling io opertion to complete
+			ioqueuer(&proc[i].arival, &proc[i].currentio);
+		}
+	//set the process completion signal 
+	if(proc[i].numcpu < proc[i].currentcpu )
+		proc[i].signal[1]=-1;
+	//now add the same to jobqueue considering arrival time and signal 
+	if(proc[i].arival<=ptime && proc[i].signal[1]==0)
+		{
+		//this gives out a pointer to minimum process
+		if(temp == NULL)
+			{
+			temp = &proc[i].cpu[proc[i].currentcpu];
+			current = i;
+			}
+		else 	
+			{
+			if(*temp > proc[i].cpu[proc[i].currentcpu])
+			temp = &proc[i].cpu[proc[i].currentcpu];
+			current = i;
+			}
+		}
+	}
+	return temp;
+}
+void ioqueuer(int *a,int *b)
+{
+	*a = *a + ptime + iowait;
+	iowait = *a + iowait;
+	*b = *b + 1;
 }
